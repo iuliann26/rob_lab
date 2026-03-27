3a1378fab786d06ebd05adf2f6a369f7f0c8c0c2
iuli <ionita.a.iulian26@gmail.com>
2026-03-28 00:33:49 +0200

    add counter publisher with csv logging

--- topics_pkg/src/simple_topic_publisher.py
+++ topics_pkg/src/simple_topic_publisher.py
@@ -1,20 +1,32 @@
-#!/usr/bin/env python
-
-
-import  rospy
+#!/usr/bin/env python3
+import rospy
 from std_msgs.msg import Int32
+import csv
 
-rospy.init_node('topic_publisher')
-pub=rospy.Publisher('/counter',Int32,queue_size=1)
+def talker():
+    rospy.init_node('topic_publisher', anonymous=True)
+    pub = rospy.Publisher('/counter', Int32, queue_size=1)
+    rate = rospy.Rate(2) 
+    count = Int32()
+    count.data = 0
+    
+    with open('counter_log.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        
+        writer.writerow(["Timestamp", "Value"])
+        csvfile.flush()
+        rospy.loginfo("Nodul a pornit. Se scrie în counter_log.csv...")
 
+        while not rospy.is_shutdown():
+    
+            pub.publish(count) 
+            writer.writerow([count.data])
+            rospy.loginfo(f"Publicat și salvat: {count.data}")           
+            count.data += 1           
+            rate.sleep()
 
-rate=rospy.Rate(2)
-count=Int32()
-count.data=0
-
-
-while not rospy.is_shutdown():
-	pub.publish(count)
-	count.data+=1
-	rate.sleep()
-
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
